6009b41d4d01d65d746d005ef5b4b16ccdc5bcb1
ShakibAhmed007 <shakib.ahmed91117@gmail.com>
2023-01-25 17:17:14 +0600

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
 import jsPDF from 'jspdf';
+import { RobotoFont } from './vfs';
 
 @Component({
   selector: 'my-app',
@@ -7,13 +8,50 @@ import jsPDF from 'jspdf';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
+  name = 'Angular JsPdf Currency Symbol Example';
+  symbol = '₹';
+  symbols = ['$', '€', '£', '¥', '₣', '₹'];
 
   @ViewChild('content') content: ElementRef;
 
-  makePdf() {
+  setTextAndSaveAsPdf() {
     const doc = new jsPDF();
-    doc.text('Hello world!   ₹ 10 101 01', 10, 10);
-    doc.save('a4.pdf');
+    doc.addFileToVFS('Roboto-Regular.ttf', RobotoFont);
+    doc.addFont('Roboto-Regular.ttf', 'Roboto-Regular', 'normal');
+    doc.setFont('Roboto-Regular');
+    doc.text(`Price =  ${this.symbol}100`, 10, 10);
+    doc.save('a.pdf');
+  }
+
+  convertHtmlToPdfAndSaveAsPdf() {
+    const doc = new jsPDF();
+    doc.addFileToVFS('Roboto-Regular.ttf', RobotoFont);
+    doc.addFont('Roboto-Regular.ttf', 'Roboto-Regular', 'normal');
+    doc.setFont('Roboto-Regular');
+
+    let html = document.getElementById('preview').innerHTML;
+    html = html.replace(/width="100%"/, 'width="565"');
+    doc.html(html, {
+      callback: function (pdf) {
+        pdf.save('b.pdf');
+      },
+      margin: [10, 15],
+    });
+  }
+
+  convertHtmlToPdfAndOpenNewWindow() {
+    const doc = new jsPDF();
+    doc.addFileToVFS('Roboto-Regular.ttf', RobotoFont);
+    doc.addFont('Roboto-Regular.ttf', 'Roboto-Regular', 'normal');
+    doc.setFont('Roboto-Regular');
+
+    let html = document.getElementById('preview').innerHTML;
+    html = html.replace(/width="100%"/, 'width="565"');
+    doc.html(html, {
+      margin: [10, 15],
+      callback: function (pdf) {
+        pdf.output('dataurlnewwindow');
+      },
+    });
   }
 }
